348e82f6a91cdc2c2e957fef4066e1f7a5bfb443
mansun <maxinesun0128@yahoo.com>
2023-06-22 19:01:16 +0200

    with better split ratio

---
+++ utils/pre_data4sort.py
@@ -0,0 +1,223 @@
+import jsonlines
+import torch.nn as nn
+from tqdm import tqdm
+import pickle
+import torch
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from torch_geometric.data import HeteroData
+import torch_geometric.transforms as T
+from torch_geometric.loader import LinkNeighborLoader
+
+
+class Data_Process:
+    def __init__(self):
+        self.mqlen = 0
+
+    def json2dataset(self, filepath):
+        # json_list = []
+        # with open(filepath, "r+") as f_in:
+        #     for json in jsonlines.Reader(f_in):
+        #         # if len(json_list) < 8000:
+        #         json_list.append(json)
+        # with open("../scratch/data/json_list", "wb") as fp:
+        #     pickle.dump(json_list, fp)
+        # fp.close()
+
+        with open("../scratch/data/json_list", "rb") as fp:
+            json_list = pickle.load(fp)
+        fp.close()
+
+        model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+        model = nn.DataParallel(model)
+        model = model.to("cuda")
+        # mq_raw = [item["main_question"] for item in json_list]
+        # mq_x = model.module.encode(mq_raw, show_progress_bar=True)
+        # with open("../scratch/data/mq_x8", "wb") as fp:
+        #     pickle.dump(mq_x, fp)
+        # fp.close()
+
+        with open("../scratch/data/mq_x", "rb") as fp:
+            mq_x = pickle.load(fp)
+        fp.close()
+
+        edge_mq_side = []
+        edge_sq_side = []
+        sq_cls_ind = 0
+        sq_node_ind = 0
+
+        with open("../scratch/data/sq_cls06", "rb") as fp:
+            sq_cls = pickle.load(fp)
+        fp.close()
+
+               
+        sq_hash = {}
+        for mq_ind, item in tqdm(enumerate(json_list)):
+            for sq_item in item["sub_questions"][0]:
+                current_cls = np.where(sq_cls == sq_cls[sq_cls_ind])
+                if sq_cls_ind == current_cls[0][0]:
+                    sq_emb = model.module.encode(sq_item)
+                    sq_hash[sq_cls_ind] = sq_node_ind
+                    sq_cls_ind += 1
+                    try:
+                        sq_x
+                        sq_x = np.vstack((sq_x, sq_emb))
+                    except NameError:
+                        sq_x = sq_emb
+                    edge_mq_side.append(mq_ind)
+                    edge_sq_side.append(sq_node_ind)
+                    sq_node_ind += 1
+                else:
+                    edge_mq_side.append(mq_ind)
+                    edge_sq_side.append(sq_hash[current_cls[0][0]])
+                    sq_cls_ind += 1
+        
+        data = HeteroData()
+        data["mq"].node_id = torch.arange(len(mq_x))
+        data["sq"].node_id = torch.arange(len(sq_x))
+        data["mq"].x = torch.from_numpy(mq_x)
+        data["sq"].x = torch.from_numpy(sq_x)
+        data["mq","queries","sq"].edge_index = torch.stack([torch.tensor(edge_mq_side), torch.tensor(edge_sq_side)], dim=0)
+        data = T.ToUndirected()(data)
+        
+
+        transform = T.RandomLinkSplit(
+            num_val=0.1,
+            num_test=0.1,
+            disjoint_train_ratio=0.3,
+            neg_sampling_ratio=2.0,
+            add_negative_train_samples=False,
+            edge_types=("mq","queries","sq"),
+            rev_edge_types=("sq","rev_queries","mq"),
+        )
+        train_data, val_data, test_data = transform(data)
+
+
+        # Define seed edges:
+        edge_label_index = train_data["mq","queries","sq"].edge_label_index
+        edge_label = train_data["mq","queries","sq"].edge_label
+
+        train_loader = LinkNeighborLoader(
+            data=train_data,
+            num_neighbors=[20, 10],
+            neg_sampling_ratio=2.0,
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=128,
+            shuffle=True,
+        )
+        with open("../scratch/data/train_loader", "wb") as fp:
+            pickle.dump(train_loader, fp)
+        fp.close()
+
+        edge_label_index = val_data["mq","queries","sq"].edge_label_index
+        edge_label = val_data["mq","queries","sq"].edge_label
+        val_loader = LinkNeighborLoader(
+            data=val_data,
+            num_neighbors=[20, 10],
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=3 * 128,
+            shuffle=False,
+        )
+        with open("../scratch/data/val_loader", "wb") as fp:
+            pickle.dump(val_loader, fp)
+        fp.close()
+        print("val loader generated")
+
+        edge_label_index = test_data["mq","queries","sq"].edge_label_index
+        edge_label = test_data["mq","queries","sq"].edge_label
+        test_loader = LinkNeighborLoader(
+            data=test_data,
+            num_neighbors=[20, 10],
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=3 * 128,
+            shuffle=False,
+        )
+
+        with open("../scratch/data/test_loader", "wb") as fp:
+            pickle.dump(test_loader, fp)
+        fp.close()
+        print("test loader generated")
+
+        with open("../scratch/data/heter_data", "wb") as fp:
+            pickle.dump(data, fp)
+        fp.close()
+
+        # return train_loader, val_loader, test_loader
+        data = HeteroData()
+        data["mq"].node_id = torch.arange(len(mq_x))
+        data["sq"].node_id = torch.arange(len(sq_x))
+        data["mq"].x = torch.from_numpy(mq_x)
+        data["sq"].x = torch.from_numpy(sq_x)
+        data["mq","queries","sq"].edge_index = torch.stack([torch.tensor(edge_mq_side), torch.tensor(edge_sq_side)], dim=0)
+        data = T.ToUndirected()(data)
+        
+
+        transform = T.RandomLinkSplit(
+            num_val=0.1,
+            num_test=0.1,
+            disjoint_train_ratio=0.3,
+            neg_sampling_ratio=2.0,
+            add_negative_train_samples=False,
+            edge_types=("mq","queries","sq"),
+            rev_edge_types=("sq","rev_queries","mq"),
+        )
+        train_data, val_data, test_data = transform(data)
+
+
+        # Define seed edges:
+        edge_label_index = train_data["mq","queries","sq"].edge_label_index
+        edge_label = train_data["mq","queries","sq"].edge_label
+
+        train_loader = LinkNeighborLoader(
+            data=train_data,
+            num_neighbors=[20, 10],
+            neg_sampling_ratio=2.0,
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=128,
+            shuffle=True,
+        )
+        with open("../scratch/data/train_loader", "wb") as fp:
+            pickle.dump(train_loader, fp)
+        fp.close()
+
+        edge_label_index = val_data["mq","queries","sq"].edge_label_index
+        edge_label = val_data["mq","queries","sq"].edge_label
+        val_loader = LinkNeighborLoader(
+            data=val_data,
+            num_neighbors=[20, 10],
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=3 * 128,
+            shuffle=False,
+        )
+        with open("../scratch/data/val_loader", "wb") as fp:
+            pickle.dump(val_loader, fp)
+        fp.close()
+        print("val loader generated")
+
+        edge_label_index = test_data["mq","queries","sq"].edge_label_index
+        edge_label = test_data["mq","queries","sq"].edge_label
+        test_loader = LinkNeighborLoader(
+            data=test_data,
+            num_neighbors=[20, 10],
+            edge_label_index=(("mq","queries","sq"), edge_label_index),
+            edge_label=edge_label,
+            batch_size=3 * 128,
+            shuffle=False,
+        )
+
+        with open("../scratch/data/test_loader", "wb") as fp:
+            pickle.dump(test_loader, fp)
+        fp.close()
+        print("test loader generated")
+
+        with open("../scratch/data/heter_data", "wb") as fp:
+            pickle.dump(data, fp)
+        fp.close()
+
+        # return train_loader, val_loader, test_loader
+        return
---
+++ utils/used/json2homo.py
@@ -0,0 +1,102 @@
+import jsonlines
+import torch.nn as nn
+from tqdm import tqdm
+import pickle
+import torch
+from sentence_transformers import SentenceTransformer
+import utils.dataset_generate as dg
+import numpy as np
+
+
+class Data_Process:
+    def __init__(self):
+        self.mqlen = 0
+
+    def json2dataset(self, filepath, datasetpath):
+        json_list = []
+        with open(filepath, "r+") as f_in:
+            for json in jsonlines.Reader(f_in):
+                # if len(json_list) < 40:
+                json_list.append(json)
+        # model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+        # model = nn.DataParallel(model)
+        # model = model.to("cuda")
+        mq_raw = [item["main_question"] for item in json_list]
+        # mq_embedding = model.module.encode(mq_raw, show_progress_bar=True)
+        # # with open("../scratch/data/mq", "rb") as fp:
+        # # mq_embedding=pickle.load(fp)
+        # x = mq_embedding
+        with open("../scratch/data/mq", "wb") as fp:
+            pickle.dump(mq_raw, fp)
+        # ind_point = len(mq_embedding) - 1
+        # print(ind_point)
+        # y = [i for i in range(ind_point + 1)]
+
+        # rq_hash = []
+        # edge_list = [[], []]
+        # for mq_ind, item in tqdm(enumerate(json_list)):
+        #     for rq_item in item["related_questions"]:
+        #         if rq_item in mq_raw:
+        #             if mq_raw.index(rq_item) not in rq_hash:
+        #                 rq_hash.append(mq_raw.index(rq_item))
+        #             edge_list[0].append(mq_ind)
+        #             edge_list[1].append(mq_raw.index(rq_item))
+        #         else:
+        #             ind_point += 1
+        #             rq_hash.append(ind_point)
+        #             edge_list[0].append(mq_ind)
+        #             edge_list[1].append(ind_point)
+        #             x.append(model.module.encode(rq_item))
+        #             y.append(ind_point)
+
+        # start_ind = ind_point
+        # with open("../scratch/data/sq_cls06", "rb") as fp:
+        #     sq_er = pickle.load(fp)
+        # sq_hash = {}
+        # for mq_ind, item in tqdm(enumerate(json_list)):
+        #     for sq_item in item["sub_questions"][0]:
+        #         current_loc = ind_point - start_ind
+        #         current_cls = np.where(sq_er == sq_er[current_loc])
+        #         if len(current_cls) > 1:
+        #             if current_cls[0] != current_loc:
+        #                 edge_list[0].append(mq_ind)
+        #                 edge_list[1].append(sq_hash[current_cls[0]])
+        #                 mq_ind = sq_hash[current_cls[0]]
+        #                 x.append(model.module.encode(sq_item))
+        #                 y.append(sq_hash[current_cls[0]])
+        #             else:
+        #                 ind_point += 1
+        #                 sq_hash[current_loc] = ind_point
+        #                 edge_list[0].append(mq_ind)
+        #                 edge_list[1].append(ind_point)
+        #                 mq_ind = ind_point
+        #                 x.append(model.module.encode(sq_item))
+        #                 y.append(ind_point)
+        #         else:
+        #             ind_point += 1
+        #             edge_list[0].append(mq_ind)
+        #             edge_list[1].append(ind_point)
+        #             mq_ind = ind_point
+        #             x.append(model.module.encode(sq_item))
+        #             y.append(ind_point)
+
+        # x = np.array(x)
+        # edge_list = np.array(edge_list)
+        # y = np.array(y)
+
+        # with open("../scratch/data/x", "rb") as fp:
+        #     x = pickle.load(fp)
+        # fp.close()
+
+        # with open("../scratch/data/ed", "rb") as fp:
+        #     edge_list = pickle.load(fp)
+        # fp.close()
+
+        # with open("../scratch/data/y", "rb") as fp:
+        #     y = pickle.load(fp)
+        # fp.close()
+        # Dataset = dg.PKG_Dataset(
+        #     datasetpath,
+        # )
+        # return Dataset
+        return
