453ebf134010407e868b6c75553eddd9aececd6a
mansun <maxinesun0128@yahoo.com>
2023-09-08 23:24:22 +0200

    sorted length

--- utils/pre_sq_unfolded.py
+++ utils/pre_sq_unfolded.py
@@ -18,11 +18,14 @@ class SentenceDataset(Dataset):
         return len(self.target_idx_set)
 
     def __getitem__(self, idx):
-        print(self.shuffled_embs[idx])
-        # encoded_sqs = [{'input_ids':item['input_ids'], 'token_type_ids': item['token_type_ids'], 'attention_mask':item['attention_mask']} for item in self.shuffled_embs[idx]]
-        # return encoded_sqs, self.target_idx_set[idx]
         return self.shuffled_embs[idx], self.target_idx_set[idx]
     
+    
+def collate_fn_len_sort(batch):
+    shuffled_embs = [item[0] for item in batch]
+    target_idx_set = [item[1] for item in batch]
+    return shuffled_embs, target_idx_set
+
 
 class Data_Process:
     def json2dataset(self, args):
@@ -31,13 +34,10 @@ class Data_Process:
         with open("../scratch/data/json_list", "rb") as fp:
             json_list = pickle.load(fp)
         fp.close()
-        # mqs = [item["main_question"] for item in json_list]
-        # encoded_mqs = tokenizer(mqs, padding=True, truncation=True, max_length=128, return_tensors='pt')
-        # sq_set = []
         sq_set_shuffled = []
         sq_set = []
         target_idx = []
-        for item in tqdm(json_list[:500]):
+        for item in tqdm(json_list):
             sq = []
             for sq_item in item["sub_questions"]:
                 sq+=sq_item
@@ -45,15 +45,9 @@ class Data_Process:
         sq_set = sorted(sq_set, key=len)
         length_sq = [len(item) for item in sq_set]
         bool_list = functional.choose_batch_idx(length_sq)
-        # print(length_sq)
-        # print(bool_list)
         sq_set = [sq_set[i] for i in range(len(sq_set)) if bool_list[i]]
-        # sq_set = sq_set[functional.choose_batch_idx(length_sq)]
-        # print(functional.find_min_continuous_length([len(item) for item in sq_set]))
-        for sq in sq_set:
+        for sq in tqdm(sq_set):
             encoded_sqs = tokenizer(sq, padding=True, truncation=True, max_length=128, return_tensors='pt')
-            # sq_set.append(encoded_sqs)
-
             shuffle_idx = list(range(len(encoded_sqs['input_ids'])))
             np.random.shuffle(shuffle_idx)
             encoded_sqs['input_ids']=encoded_sqs['input_ids'][shuffle_idx]
@@ -64,61 +58,28 @@ class Data_Process:
             shuffle_idx = torch.tensor(shuffle_idx)
             perm_ground_truth = torch.nn.functional.one_hot(torch.argsort(shuffle_idx, dim=-1)).transpose(-2, -1).float()
             target_idx.append(perm_ground_truth)
-        
-        # # with open("../scratch/data/diff_sort/encoded_mqs", "wb") as fp:
-        # #     pickle.dump(encoded_mqs, fp)
-        # # fp.close()
-        # # with open("../scratch/data/diff_sort/sq_set", "wb") as fp:
-        # #     pickle.dump(sq_set_shuffled, fp)
-        # # fp.close()
-        # with open("../scratch/data/diff_sort/sq_set_shuffled", "wb") as fp:
-        #     pickle.dump(sq_set_shuffled, fp)
-        # fp.close()
-
-        # with open("../scratch/data/diff_sort/target_idx", "wb") as fp:
-        #     pickle.dump(target_idx, fp)
-        # fp.close()
-
-        # with open("../scratch/data/diff_sort/encoded_mqs", "rb") as fp:
-        #     encoded_mqs = pickle.load(fp)
-        # fp.close()
-        # with open("../scratch/data/diff_sort/sq_set_shuffled", "rb") as fp:
-        #     sq_set_shuffled = pickle.load(fp)
-        # fp.close()
-        # with open("../scratch/data/diff_sort/target_idx", "rb") as fp:
-        #     target_idx = pickle.load(fp)
-        # fp.close()
-
-
-        
 
         dataset_size = len(target_idx)
         train_size = int(0.8 * dataset_size / 16) * 16
-        val_size = int(0.1 * dataset_size/ 16) * 16
+        val_size = int(0.1 * dataset_size / 16) * 16
+        print(dataset_size)
+        print(train_size)
+        print(val_size)
         dataset = SentenceDataset(sq_set_shuffled, target_idx)
-        # train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
         train_dataset = dataset[:train_size]
         val_dataset = dataset[train_size:train_size + val_size]
         test_dataset = dataset[train_size + val_size:]
-        print(val_dataset)
-        train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=False)
-        val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False)
-        test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-        
-        for item in train_dataloader:
-            print(item)
-        
-        
-        
-        
+        train_dataloader = DataLoader(train_dataset, batch_size=16, collate_fn=collate_fn_len_sort, shuffle=False) 
+        val_dataloader = DataLoader(val_dataset, batch_size=16, collate_fn=collate_fn_len_sort, shuffle=False)
+        test_dataloader = DataLoader(test_dataset, batch_size=16, collate_fn=collate_fn_len_sort, shuffle=False)
                 
-        # with open("../scratch/data/diff_sort/train_dataloader", "wb") as fp:
-        #     pickle.dump(train_dataloader, fp)
-        # fp.close()
-        # with open("../scratch/data/diff_sort/val_dataloader", "wb") as fp:
-        #     pickle.dump(val_dataloader, fp)
-        # fp.close()
-        # with open("../scratch/data/diff_sort/test_dataloader", "wb") as fp:
-        #     pickle.dump(test_dataloader, fp)
-        # fp.close()
+        with open("../scratch/data/diff_sort/train_dataloader", "wb") as fp:
+            pickle.dump(train_dataloader, fp)
+        fp.close()
+        with open("../scratch/data/diff_sort/val_dataloader", "wb") as fp:
+            pickle.dump(val_dataloader, fp)
+        fp.close()
+        with open("../scratch/data/diff_sort/test_dataloader", "wb") as fp:
+            pickle.dump(test_dataloader, fp)
+        fp.close()
 
